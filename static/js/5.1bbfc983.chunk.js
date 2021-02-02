c4a6f03d0241b7668ccf4b645ea0710ffb3b71bd
Wiktor Starczewski <poszerny@gmail.com>
2021-02-02 20:09:50 +0100

    Updates

---
+++ static/js/5.1bbfc983.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-react-app"]=this["webpackJsonpmy-react-app"]||[]).push([[5],{692:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return c}));var o=e(343),c=(e(96),function(){var n=window;n.addEventListener("statusTap",(function(){Object(o.g)((function(){var t=n.innerWidth,e=n.innerHeight,c=document.elementFromPoint(t/2,e/2);if(c){var i=c.closest("ion-content");i&&i.componentOnReady().then((function(){Object(o.m)((function(){return i.scrollToTop(300)}))}))}}))}))})}}]);
+//# sourceMappingURL=5.1bbfc983.chunk.js.map
